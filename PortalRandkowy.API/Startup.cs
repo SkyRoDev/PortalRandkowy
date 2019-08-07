4246ce0d75937780fe2a96312988b24d931c1928
Maciej <mzubrzycki@op.pl>
2019-08-07 10:57:10 +0200

    Utworzenie modelu Value

--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PortalRandkowy.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace PortalRandkowy.API
 {
@@ -25,6 +28,7 @@ namespace PortalRandkowy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x =>x.UseSqlite("Conectionstring"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
