bd949109aacdbfaaead18140a4cb04f854ef44f7
Maciej <mzubrzycki@op.pl>
2019-08-13 08:21:14 +0200

    Add a sesion token for API

--- PortalRandkowy.API/Controllers/AuthController.cs
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Dtos;
 using PortalRandkowy.API.Models;
@@ -11,9 +17,11 @@ namespace PortalRandkowy.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
-        public AuthController(IAuthRepository repository)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
-            _repository = repository;            
+            _config = config;
+            _repository = repository;
         }
 
         [HttpPost("register")]
@@ -21,10 +29,11 @@ namespace PortalRandkowy.API.Controllers
         {
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _repository.UserExists(userForRegisterDto.Username))
+            if (await _repository.UserExists(userForRegisterDto.Username))
                 return BadRequest("Użytkownik o takiej nazwie już isteniej");
 
-            var userToCreate = new User{
+            var userToCreate = new User
+            {
                 UserName = userForRegisterDto.Username
             };
 
@@ -32,6 +41,36 @@ namespace PortalRandkowy.API.Controllers
 
             return StatusCode(201);
         }
-        
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFormRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            //Is User exis
+            if (userFormRepo == null)
+                return Unauthorized();
+
+            //Create token for logi
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier,userFormRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFormRepo.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescryptor = new SecurityTokenDescriptor{
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+            
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescryptor);
+
+            return Ok(new {token = tokenHandler.WriteToken(token)});
+
+        }
+
     }
 }
